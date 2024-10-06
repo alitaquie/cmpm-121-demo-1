d264575765903a7ba160c36ac5ad4bfedfa1e652
ali <alitaquie51@gmail.com>
2024-10-06 16:12:01 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
+const display: HTMLDivElement = document.querySelector("#display_kicks")!;
 
-const buttonName = "⚽ "
+const buttonName = "⚽"
 const button = document.createElement('button');
-button.innerHTML = buttonName
+button.innerHTML = buttonName;
+
+let kicks: number = 0;
+display.innerText = `Kicks: ${kicks}`;
+
+const kick_ball = () => {
+  kicks += 1;
+  display.innerText = `Kicks: ${kicks}`;
+};
 
 
+button.addEventListener('click', kick_ball);
 
 app.append(button);
+app.append(display);
